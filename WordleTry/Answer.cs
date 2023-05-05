0b63e3f61f3ed7cfac20709257d0ebf4ced5231a
bspaid44 <103225608+bspaid44@users.noreply.github.com>
2023-05-05 09:26:38 -0400

    Add files via upload

---
+++ WordleTry/Answer.cs
@@ -0,0 +1,77 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleTry
+{
+
+    public enum LetterStatus
+    {
+        Empty = 0,          // "This space does not contain a letter."
+        Wrong = 1,          // "This letter is not in the word."
+        WrongPosition = 2,  // "This letter is in the word, but not in this position."
+        Correct = 3         // "This letter is in the word in this position."
+    }
+
+    public class Aback
+    {
+        public string word { get; set; }
+        public int index { get; set; }
+
+        public static string ReadCsv()
+        {
+            using (var reader = new StreamReader("word-bank.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var record = new Aback();
+                var records = csv.GetRecords<Aback>();
+
+                string error = "Word cannot be found";
+                Random random = new Random();
+                int rand_num = random.Next(0, 2313);
+
+                foreach (var r in records)
+                {
+                    
+                    string word = r.word;
+                    int index = r.index;
+                    
+                    if (rand_num == index)
+                    {
+                        return word;
+                    }
+
+                } return error;
+            }
+        }
+
+        
+
+        public static LetterStatus[] CheckLetters(string answer, string guess)
+        {
+            var result = new LetterStatus[5];
+
+            for (int i = 0; i < guess.Length; i++)
+            {
+                if (answer[i] == guess[i])
+                {
+                    result[i] = LetterStatus.Correct;
+                }
+                else if (answer.Contains(guess[i]))
+                {
+                    result[i] = LetterStatus.WrongPosition;
+                }
+                else
+                {
+                    result[i] = LetterStatus.Wrong;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ WordleTry/Program.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WordleTry
+{
+    internal class Program
+    {
+        public string guess;
+        public string answer;
+        
+
+        
+
+        static void Main(string[] args)
+        {
+            string answer = Aback.ReadCsv();
+
+            Console.WriteLine("Welcome to totally not copywrited Wordle");
+            Console.WriteLine("\n\n");
+
+            int i = 0;
+
+
+            while (i == 0)
+            {
+                Console.WriteLine("\nPlease type your five-letter guess in: \n");
+                string guess = Console.ReadLine();
+
+                var results = Aback.CheckLetters(answer, guess);
+
+                foreach (var result in results)
+                {
+                    Console.Write(result + " ");
+
+                    if (answer == guess)
+                    {
+                        Console.WriteLine("You win!!!");
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
